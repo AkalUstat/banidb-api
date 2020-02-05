9599499c1eed64c168309563d87ee169aec6f65e
Taranjit Singh <tsingh777@gmail.com>
2020-02-04 18:41:38 -0800

    Demo for swagger docs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const cacheControl = require('express-cache-controller');
 const routes = require('./api/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const port = process.env.NODE_ENV === 'development' ? '3001' : '3000';
@@ -12,6 +14,7 @@ app.use(cacheControl({ maxAge: 21600 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/v2', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.cacheControl = { noCache: true };
